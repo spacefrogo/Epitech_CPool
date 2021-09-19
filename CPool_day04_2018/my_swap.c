caacbf2d9364c0c53337eb579c1431ec9e71af9e
spacefrogo <valton.gara@gmail.com>
2021-09-19 16:03:18 +0200

    update

---
+++ CPool_day04_2018/my_swap.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** my_swap.c
+** File description:
+** my_swap.c
+*/
+
+void	my_swap(int *a, int *b)
+{
+    int vlera;
+    vlera = *a;
+    *a = *b;
+    *b = vlera;
+}
